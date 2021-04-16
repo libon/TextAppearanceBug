12cc66e125f29c8525b51979bf1a549498e8d5e2
Carmen Alvarez <calvarez@libon.com>
2021-04-16 12:29:35 +0200

    Add the code to demo the bug:
    Add three `TextViews`.
    
    * Apply different combinations of `TextAppearance` and `Typeface` to each.
    * Make their texts explain what's applied and the expected outcome.

--- app/src/main/java/com/example/textappearancebug/MainActivity.kt
+++ app/src/main/java/com/example/textappearancebug/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.textappearancebug
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text1 = findViewById<TextView>(R.id.text1)
+        val text2 = findViewById<TextView>(R.id.text2)
+        val text3 = findViewById<TextView>(R.id.text3)
+
+        text1.setTextAppearance(R.style.TextAppearanceBold)
+
+        text2.setTextAppearance(R.style.TextAppearanceBold)
+        text2.setTypeface(null, Typeface.NORMAL)
+
+        text3.setTextAppearance(R.style.TextAppearanceBold)
+        text3.setTypeface(null, Typeface.NORMAL)
+        text3.setTextAppearance(R.style.TextAppearanceEmpty)
     }
 }
